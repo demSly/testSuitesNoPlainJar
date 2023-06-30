03216c9621ce5ec840d5f31a2dca9b432300ad46
demSly <43607658+demSly@users.noreply.github.com>
2023-06-30 08:20:25 +0200

    fix

--- src/e2eTest/java/com/example/testsuitepoc/MyE2ETest.java
+++ src/e2eTest/java/com/example/testsuitepoc/MyE2ETest.java
@@ -2,7 +2,6 @@ package com.example.testsuitepoc;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
-import com.other.ExternalClass;
 import org.junit.jupiter.api.Test;
 
 class MyE2ETest {
@@ -10,7 +9,6 @@ class MyE2ETest {
   @Test
   void test(){
     FromMain fromMain = new FromMain();
-    ExternalClass externalClass = new ExternalClass();
   }
 
 }
